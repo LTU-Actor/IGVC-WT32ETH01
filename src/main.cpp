bc444f4adfe4665c58eaadaad795c8b17a045fec
rkaddis <rkaddis@ltu.edu>
2025-01-09 12:14:50 -0500

    mqtt basic working

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,38 @@
 #include <Arduino.h>
 #include <ETH.h>
-#include <PubSubClient.h>
+#include "MQTT.h"
 
 // Ethernet Setup Addresses
 IPAddress deviceIP(10,42,0,10);
 IPAddress deviceGW(10,42,0,1);
 IPAddress deviceSN(255,255,255,0);
-IPAddress deviceDNS(8,8,8,8);
+IPAddress deviceDNS = deviceIP;
 
 // MQTT Setup
-PubSubClient client;
-IPAddress server(10,42,0,1);
+MQTTClient client;
+IPAddress mqttAddress(10,42,0,4);
+WiFiClient wc;
+
+int lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (!ETH.linkUp()) {
+    Serial.print("!");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("wt32")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  // client.subscribe("/hello");
+  // client.unsubscribe("/hello");
+}
 
 
 void setup() {
@@ -22,20 +44,25 @@ void setup() {
   //Ethernet networking setup
   ETH.begin(ETH_PHY_ADDR, ETH_PHY_POWER);
   ETH.config(deviceIP, deviceGW, deviceSN, deviceDNS);
-  
-  WiFiClient wt32client;
-  client = PubSubClient(wt32client);
-  client.setServer(server, 1883);
 
-  
+  client.begin(mqttAddress, 1883, wc);
+  connect();
   
 }
 
 void loop() {
-  if(client.connect("wt32")) {
-    client.publish("test", "hello mqtt!");
-  } 
-  delay(1000);
+  client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    client.publish("/hello", "world");
+  }
   
   
 }
