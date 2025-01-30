074a457edf4fde53067e32bbf0c49a72494d90ee
rkaddis <rkaddis@ltu.edu>
2025-01-30 14:16:47 -0500

    class-less MQTT refactor

--- include/WheelController.h
+++ include/WheelController.h
@@ -8,105 +8,55 @@
 #include "MQTT.h"
 
 // Pin definitions
-#define POWER_PIN 14    // Pin used for wheel throttle power
-#define BRAKE_PIN 15    // Pin used for wheel braking toggle
-#define STEER_PIN 17    // Pin used for steering angle
-#define ESTOP_IN_PIN 36    // Pin used to take observe E-Stop loop
+#define POWER_PIN 14    // wheel throttle power
+#define BRAKE_PIN 5     // wheel braking toggle
+#define STEER_PIN 17    // steering angle
+#define REVERSE_PIN 32  // reversing wheel direction
+#define ESTOP_IN_PIN 35 // observing E-Stop loop
+#define HALLA_PIN 15    // Hall A
+#define HALLB_PIN 36    // Hall B
+#define HALLC_PIN 39    // Hall C
+
 
 #define MESSAGE_BUFFERSIZE 1024   // Maximum MQTT message size, in bytes
 
 #define ESTOP_TIMEOUT_MILLIS 50     // Number of milliseconds to wait between messages to trigger an estop
 
+// power pin callback, sends PWM to wheel
+void powerCb(String& msg) {
+    int pwm = msg.toInt();
+    analogWrite(pwm, POWER_PIN);
+}
 
-class WheelController {
+// steer pin callback, sends PWM to steer servo
+void steerCb(String& msg) {
+    int pwm = msg.toInt();
+    analogWrite(pwm, STEER_PIN);
+}
 
-    private:
+// brake pin callback, sends digital to brake channel
+void brakeCb(String& msg) {
+    int pwm = (msg.toInt() == 0) ? 0 : 255;
+    analogWrite(pwm, BRAKE_PIN);
+}
 
-        MQTTClient client;
-        bool softwareEstop;     // whether the software estop was triggered
-        bool hardwareEstop;     // whether the hardware estop was triggered
-        int timeout;            // timeout millis counter
-        String estopDebug;
+void topicCb(String& topic, String& msg) {
+    // this->estopCb(msg);
+}
 
-        // whether the bot should be e-stop
-        bool estop() {
-            return (softwareEstop || hardwareEstop);
-        }
+// establishes client connection to MQTT and sets callback
+bool mqttConnect(const char* name, MQTTClient& client, void *callback(String&, String&)) {
+    if (!ETH.linkUp()) {
+        return false;
+    }
 
-        // power pin callback, sends PWM to wheel
-        void powerCb(String& topic, String& msg) {
-            if(estop) { return; }
-            int pwm = msg.toInt();
-            analogWrite(pwm, POWER_PIN);
-            this->timeout = ESTOP_TIMEOUT_MILLIS;
-        }
+    if (!client.connect(name)) {
+        return false;
+    }
 
-        // steer pin callback, sends PWM to steer servo
-        void steerCb(String& topic, String& msg) {
-            if(estop) { return; }
-            int pwm = msg.toInt();
-            analogWrite(pwm, STEER_PIN);
-            this->timeout = ESTOP_TIMEOUT_MILLIS;
-        }
-
-        // brake pin callback, sends digital to brake channel
-        void brakeCb(String& topic, String& msg) {
-            if(estop) { return; }
-            int pwm = (msg.toInt() == 0) ? 0 : 255;
-            analogWrite(pwm, BRAKE_PIN);
-            this->timeout = ESTOP_TIMEOUT_MILLIS;
-        }
-
-        // estop message callback
-        void estopCb(String& topic, String& msg) {
-            this->softwareEstop = true;
-        }
- 
-        void connect() {
-            // TODO: attempt to connect to MQTT, setup pub/sub
-        }
-
-        // pin logic for estop 
-        void checkEStopLoop() {
-            if(analogRead(ESTOP_IN_PIN) < 127) {    // if the estop loop is low
-                this->hardwareEstop = true;
-            }
-            else {
-                this->hardwareEstop = false;
-            }
-            if(estop()) {
-                analogWrite(255, BRAKE_PIN);    // turn on brake
-            }
-            else {
-                this->timeout = (this->timeout > 0) ? this->timeout-1 : 0;
-                if(this->timeout == 0) {
-                    this->softwareEstop = true;
-                }
-            }
-        }
-
-    public:
-
-        WheelController(IPAddress mqttAddress, int port, WiFiClient wc) {
-            this->client = MQTTClient(MESSAGE_BUFFERSIZE);
-            this->client.begin(mqttAddress, port, wc);
-        }
-
-        // MQTT and estop check loop
-        void loop() {
-            if(!this->client.connected()) {
-                this->softwareEstop = true;
-                connect();
-            }
-            else {
-                this->client.loop();
-                checkEStopLoop();
-            }
-            delay(1);
-        }
-
-
-};
+    client.onMessage(callback);
+    return true;
+}
 
 
 
