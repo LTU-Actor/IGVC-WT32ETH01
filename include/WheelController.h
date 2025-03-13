2a09510757038d819e9404eb5ddea852fba753de
rkaddis <rkaddis@ltu.edu>
2025-03-13 12:40:45 -0400

    steering motor, encoder, hall working

--- include/WheelController.h
+++ include/WheelController.h
@@ -6,18 +6,31 @@
 #include <Arduino.h>
 #include <ETH.h>
 #include "MQTT.h"
+#include "CytronMotorDriver.h"
+#include "AS5600.h"
+#include "SimpleFOC.h"
 
 // MQTT client name
 #define CLIENT_NAME "testboard"
 
-// Pin definitions
-#define POWER_PIN 14    // wheel throttle power
-#define BRAKE_PIN 15    // wheel braking toggle
-#define STEER_PIN 17    // steering angle
-#define REVERSE_PIN 4   // reversing wheel direction
+// Linear motor controller pin definitions
+#define POWER_PIN  4   // wheel throttle power
+#define POWER_DIR_PIN 5   // wheel throttle direction
+#define BRAKE_PIN  3   // wheel braking toggle
+#define HALL_A_PIN 12 // hall sensor A
+#define HALL_B_PIN 14 // hall sensor B
+#define HALL_C_PIN 15 // hall sensor C
+
+
+// Steering motor controller pin definitions
+#define STEER_PIN 2    // steering power
+#define STEER_DIR_PIN 17 // steering direction
+#define ENC_SCL_PIN 32 // steering encoder scl
+#define ENC_SDA_PIN 33 // steering encoder sda
+
+
 
 #define POWER_PWM_CHANNEL 0 // LEDC Channel for power
-#define STEER_PWM_CHANNEL 1 // LEDC Channel for steering
 
 // MQTT definitions
 #define MESSAGE_BUFFERSIZE 1024   // Maximum MQTT message size, in bytes
@@ -25,48 +38,48 @@
 #define BRAKE_TOPIC "brake" // mqtt topic name for braking
 #define STEER_TOPIC "steer" // mqtt topic name for steering
 #define DEBUG_TOPIC "debug" // mqtt topic name for debug info
-
-
+#define ENC_DEBUG_TOPIC "encoder" // mqtt topic name for encoder debug info
+#define HALL_VEL_TOPIC "velocity" // wheel velocity
+#define HALL_ANGLE_TOPIC "angle" // wheel angle
 
 #define ESTOP_TIMEOUT_MILLIS 50     // Number of milliseconds to wait between messages to trigger an estop
 
-extern MQTTClient client(MESSAGE_BUFFERSIZE);
-extern int timeout = 0;
+extern MQTTClient client(MESSAGE_BUFFERSIZE); // mqtt client
+extern int timeout = 0; // time in millis until controls time out
 
-bool direction = true;
+CytronMD steerController(PWM_DIR, STEER_PIN, STEER_DIR_PIN); // steering controller for cytron MC
+extern AS5600 as5600(&Wire); // encoder wire 0
+extern HallSensor hall(HALL_A_PIN, HALL_B_PIN, HALL_C_PIN, 14);
 
-// publishes a message on to the debug topic
-void debug(const String& msg) { 
+int target_speed = 0; // wheel target speed
+int target_angle = 0; // wheel target angle
+
+void pub(const String& msg, const char* topic) {
     if(!client.connected()) {
         return;
     }
-    client.publish(("/" + String(CLIENT_NAME) + "/" + DEBUG_TOPIC), msg);
+    client.publish(("/" + String(CLIENT_NAME) + "/" + topic), msg);
 }
+
+// publishes a message on to the debug topic
+void debug(const String& msg) { 
+    pub(msg, DEBUG_TOPIC);
+}
+
+
 
 // power pin callback, sends PWM to wheel power
 void powerCb(String& msg) {
-    int pwm = msg.toInt();
-    if(!direction && pwm >= 0) {
-        direction = true;
-        analogWrite(REVERSE_PIN, 0);
-        ledcWrite(POWER_PWM_CHANNEL, 0);
-        delay(10);
-    }
-    else if(direction && pwm < 0) {
-        direction = false;
-        analogWrite(REVERSE_PIN, 255);
-        ledcWrite(POWER_PWM_CHANNEL, 0);
-        pwm *= -1;
-        delay(10);
-    }
-    ledcWrite(POWER_PWM_CHANNEL, pwm);
+    // TODO: Use hall feedback to do smooth acceleration and speed control
+    target_speed = msg.toInt();
     
 }
 
-// steer pin callback, sends PWM to steer servo
+// steer pin callback, sends PWM to steer motor
 void steerCb(String& msg) {
-    int pwm = msg.toInt();
-    ledcWrite(STEER_PWM_CHANNEL, pwm);
+    // TODO: Use the Cytron package to arrive at steering angle with PID control
+    target_angle = msg.toInt();
+    steerController.setSpeed(target_angle);
     
 }
 
@@ -110,7 +123,28 @@ bool mqttConnect(MQTTClient& client, void callback(String&, String&)) {
 }
 
 
+void steerLoop() {
+    int wire0 = as5600.readAngle();
+    pub(String(wire0), ENC_DEBUG_TOPIC);
+}
 
+void doA(){hall.handleA();}
+void doB(){hall.handleB();}
+void doC(){hall.handleC();}
+
+void hallLoop() {
+    hall.update();
+    double vel = hall.getVelocity();
+    double angle = hall.getAngle();
+    pub(String(vel), HALL_VEL_TOPIC);
+    pub(String(angle), HALL_ANGLE_TOPIC);
+
+    int a = digitalRead(HALL_A_PIN);
+    int b = digitalRead(HALL_B_PIN);
+    int c = digitalRead(HALL_C_PIN);
+
+    pub(String("A: ") + String(a) + String("\nB: ") + String(b) + String("\nC: ") + String(c), "hall");
+}
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,27 @@ const IPAddress mqttAddress(192,168,0,3);
 WiFiClient wc;
 
 
-
 // sets up pins for input/output
 void pinSetup() {
     ledcSetup(POWER_PWM_CHANNEL, 40000, 8);
     ledcAttachPin(POWER_PIN, POWER_PWM_CHANNEL);
-    ledcSetup(STEER_PWM_CHANNEL, 40000, 8);
-    ledcAttachPin(STEER_PIN, STEER_PWM_CHANNEL);
+    pinMode(POWER_DIR_PIN, OUTPUT);
     pinMode(BRAKE_PIN, OUTPUT);
-    pinMode(REVERSE_PIN, OUTPUT);
+    pinMode(STEER_PIN, OUTPUT);
+    pinMode(STEER_DIR_PIN, OUTPUT);
+
+    Wire.begin();
+    as5600.begin(ENC_SCL_PIN);  //  set direction pin.
+    as5600.setDirection(AS5600_CLOCK_WISE);  // default, just be explicit.
+
+    // pinMode(HALL_A_PIN, INPUT);
+    // pinMode(HALL_B_PIN, INPUT);
+    // pinMode(HALL_C_PIN, INPUT);
+    hall.pullup = Pullup::USE_INTERN;
+    hall.init();
+    hall.enableInterrupts(doA, doB, doC);
+    _delay(1000);
+   
 }
 
 
@@ -31,6 +43,7 @@ void setup() {
   while(!Serial);
 
   pinSetup();
+  
 
   // Ethernet networking setup
   
@@ -61,6 +74,10 @@ void loop() {
     client.loop();
   }
 
+  // hall.update();
+  steerLoop();
+  hallLoop();
+  
   timeout = max(0, timeout - LOOP_DELAY_MS);
   delay(LOOP_DELAY_MS);
   
